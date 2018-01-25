a14de83dae33a8128602da734bfe90d5466efac1
Jerry <11736648@qq.com>
2018-01-25 18:52:15 +0800

    text

--- support/__init__.py
+++ support/__init__.py
@@ -1,3 +1,4 @@
+#-*- coding:utf-8 -*-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security, SQLAlchemyUserDatastore
--- support/models/gateway_tianfutong.py
+++ support/models/gateway_tianfutong.py
@@ -5,14 +5,14 @@ class Param(db.Model):
     __tablename__ = 'common_info'
 
     common_info_id = db.Column(db.Integer, primary_key=True)
-    device_id = db.Column(db.Integer, )
-    biz_serial_no
-    imei
-    cplc
-    mobile_number
-    rom_version
-    api_level
-    ws_version
-    package_version_code
-    create_timestamp
-    update_timestamp
+    device_id = db.Column(db.Integer, nullable= False)
+    biz_serial_no = db.Column(db.String(48), nullable= False, unique= True)
+    imei = db.Column(db.String(32))
+    cplc = db.Column(db.String(84), nullable= False)
+    mobile_number = db.Column(db.String(11))
+    rom_version = db.Column(db.String(128))
+    api_level = db.Column(db.String(32))
+    ws_version = db.Column(db.String(64))
+    package_version_code = db.Column(db.String(16))
+    create_timestamp = db.Column(db.datetime, nullable=False, server_default=db.FetchedValue())
+    update_timestamp = db.Column(db.datetime, nullable=False, server_default=db.FetchedValue())
